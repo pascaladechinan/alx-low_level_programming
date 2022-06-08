173eda0d83ba731fd028b470d954498a945b3623
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-08 12:55:59 +0100

    Update and rename 0-putchar.c.swp to 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* main - Prints numbers between 00 to 89.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
