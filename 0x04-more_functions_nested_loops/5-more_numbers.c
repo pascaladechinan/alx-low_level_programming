039c5c6097397074b201e4bd5712a0a007c50c00
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-09 06:59:55 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				putchar((j / 10) + '0');
+			}
+			putchar((j % 10) + '0');
+		}
+		putchar(10);
+	}
+}
