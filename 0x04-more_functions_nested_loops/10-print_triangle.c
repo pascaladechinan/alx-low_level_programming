1b7953ee1604fd5c9d6a48684740154f9ee7ffa8
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-09 07:08:08 +0100

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle
+ *@size:size parameter of triangle
+ *Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+    int i, j;
+
+    if (size > 0)
+    {
+        for (i = 1; i <= size; i++)
+        {
+            for ((j = size - i); j > 0; j--)
+            {
+                putchar(' ');
+            }
+
+            for (j = 0; j < i; j++)
+            {
+                putchar('#');
+            }
+
+            if (i == size)
+            {
+                continue;
+            }
+
+            putchar('\n');
+        }
+    }
+    putchar('\n');
+}
