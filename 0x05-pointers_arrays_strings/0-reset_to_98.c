4be3e77409ac8153ee7184de149c7390a39ec68a
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-13 04:50:00 +0100

    Update 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - update and interger value to 98
+ * @n: A pointer to an int that will be updated
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
