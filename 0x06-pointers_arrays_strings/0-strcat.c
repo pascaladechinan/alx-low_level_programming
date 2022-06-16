91dd4bd7677bef340d698a548b40593941e95199
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-16 08:00:00 +0100

    Update 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: Second string
+ * @src: first string
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strncat(char *dest, char *src)
+{
+int x, y;
+
+for (x = 0; dest[x] != '\0'; x++)
+;
+for (y = 0; src[y] != '\0'; y++)
+{
+dest[x] = src[y];
+x++;
+}
+dest[x] = '\0';
+return (dest);
+}
