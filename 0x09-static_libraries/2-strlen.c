059a16ab91869cffe2417568dfdbb87e72b46cfa
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-28 07:23:07 +0200

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: string to find length
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	continue;
+	}
+	return (i);
+}
