58fddc63396161b1c266f1a74b094443b4c5032a
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-28 07:38:57 +0200

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ *@c: The chacter to be checked
+ * Return: 1 if a character is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+
+	return (1);
+	else
+	return (0);
+
+}
