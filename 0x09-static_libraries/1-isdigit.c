c84032cdcb069abd3cd130cc0398badb9c5ecf80
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-06-28 07:05:52 +0200

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - evaluates if its a digit
+ * @c: var to be evaluated
+ * Return: 1 if a digit 0  otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
